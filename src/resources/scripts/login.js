1a1f4da87f59d08b19911bc62f6c13fb7823aa73
Sam-MARTis <samanthmartis@gmail.com>
2024-02-04 19:18:15 +0530

    testing api call

--- src/resources/scripts/login.js
+++ src/resources/scripts/login.js
@@ -3,6 +3,28 @@ const CheckUser = () => {
     let password = document.getElementById('password').value;
     let password2 = document.getElementById('password2').value;
     let err = document.getElementById('passwordConfirm');
+    fetch('http://127.0.0.1:8000/new-user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 'name': "Samanth", 'email:':"samanthmartis@gmail.com", 'password': "1234"}),
+    })
+    .then(data => {
+        console.log(`API data is: ${data}`);
+    //     if( data.outcome == '1' ) {
+    //         console.log("Recieved 1")
+
+
+    // }
+    // else{
+    //     console.log("Recieved 2")
+    // }
+    }
+    )
+    .catch(err => {
+        console.log(err);
+    });
     if(parseInt(email).toString()===email && email.length==10){
         console.log('same');
     
@@ -19,5 +41,9 @@ const CheckUser = () => {
     } else {
         err.innerHTML = "Invalid Entry<br><br>";
     }
+
+
+    
+    
 
 }
