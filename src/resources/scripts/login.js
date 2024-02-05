5fd392319ccfe2c97add95c8618cb7337d25bc6f
Sam-MARTis <samanthmartis@gmail.com>
2024-02-05 19:18:26 +0530

    login/signup pages

--- src/resources/scripts/login.js
+++ src/resources/scripts/login.js
@@ -1,37 +1,30 @@
-const CreateUser = () => {
+const LoginUser = () => {
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
-    let password2 = document.getElementById('password2').value;
-    let err = document.getElementById('passwordConfirm');
+    let errbox = document.getElementById('allgood');
     
-    if(parseInt(email).toString()===email && email.length==10){
-        console.log('same');
+        // console.log('same');
     
-    // console.log( parseInt(email));
-        if (password===password2) {
-            if (password2.length>=6){
-                    console.log(`emailile number is: ${email}\nPassword is: ${password}`)
-                    err.innerHTML = "";
 
-                    let dataVal = -1;
-                    fetch('http://127.0.0.1:8000/new_user', {
+                    fetch('http://127.0.0.1:8000/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
                     },
-                    body: JSON.stringify({ 'username': 1, 'password': 1}),
+                    body: JSON.stringify({ 'email': email, 'password': password}),
                     })
                     .then(res => res.json())
                     .then(data => {
                         console.log(data);
                         
                         if(data==1){
-                            console.log("User exists")
+                            console.log("Logging in")
                         }
                         else if(data==0){
-                            console.log("User created");
+                            console.log("Incorrect password");
                         }
-                        else console.log("Uncaught Error");
+                        else console.log("Invalid credentials");
+                        errbox.innerHTML = "Invalid credentials";
 
 
 
@@ -40,22 +33,12 @@ const CreateUser = () => {
                     .catch(err => {
                         console.log(err);
                     });
-                }
-                else err.innerHTML = "Password too short<br><br>";
-
-
-
-        } else {
-            
-            err.innerHTML = "Passwords must match<br><br>";
-            console.log("error");
-        }
-    } else {
-        err.innerHTML = "Invalid Entry<br><br>";
-    }
-
-
-    
-    
-
+                
+                
+                
 }
+
+
+
+    
+    
--- src/resources/scripts/signup.js
+++ src/resources/scripts/signup.js
@@ -14,12 +14,12 @@ const CreateUser = () => {
                     err.innerHTML = "";
 
                     let dataVal = -1;
-                    fetch('http://127.0.0.1:8000/new_user', {
+                    fetch('http://127.0.0.1:8000/new-user', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
                     },
-                    body: JSON.stringify({ 'username': 1, 'password': 1}),
+                    body: JSON.stringify({ 'email': email, 'password': password}),
                     })
                     .then(res => res.json())
                     .then(data => {
@@ -27,6 +27,7 @@ const CreateUser = () => {
                         
                         if(data==1){
                             console.log("User exists")
+                            window.open('login.html', '_self');
                         }
                         else if(data==0){
                             console.log("User created");
