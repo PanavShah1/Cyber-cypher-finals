37f82ddff8af941b240be81f9afb9635d1481677
PanavShah1 <panav.shah@gmail.com>
2024-02-08 23:50:25 +0530

    databasing

--- src/resources/data/create_database.py
+++ src/resources/data/create_database.py
@@ -25,3 +25,15 @@ sql = """
     PRIMARY KEY (`id`));
 """
 mycursor.execute(sql)
+
+"""
+CREATE TABLE `cyber_cypher`.`doctors` (
+  `name` INT NULL,
+  `password` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`email`));
+  ALTER TABLE `cyber_cypher`.`doctors` 
+ADD COLUMN `speciality` VARCHAR(45) NULL AFTER `email`;
+ALTER TABLE `cyber_cypher`.`doctors` 
+CHANGE COLUMN `name` `name` VARCHAR(45) NULL DEFAULT NULL ;
+"""
